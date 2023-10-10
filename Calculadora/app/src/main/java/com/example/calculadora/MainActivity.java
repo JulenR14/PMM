c97916af37af7c19f2abd826cd94b00e759a11c8
JulenR14 <julen20191414@gmail.com>
2023-10-11 00:59:18 +0200

    App terminada
    Falta mejorar la App pero funciona correctamente.

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -8,12 +8,16 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    //atributos
     String muestraOperacion = "";
+    int totalSuma = 0;
     private ArrayList<String> operacion = new ArrayList<String>();
-    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnMas, btnIgual, btnC;
     private TextView textoOperacion;
 
     @Override
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //instanciamos todos los Objetos
         btn0 = findViewById(R.id.button0);
         btn1 = findViewById(R.id.button1);
         btn2 = findViewById(R.id.button2);
@@ -31,9 +36,13 @@ public class MainActivity extends AppCompatActivity {
         btn7 = findViewById(R.id.button7);
         btn8 = findViewById(R.id.button8);
         btn9 = findViewById(R.id.button9);
-
+        btnMas = findViewById(R.id.buttonMas);
+        btnIgual = findViewById(R.id.buttonIgual);
+        btnC = findViewById(R.id.buttonC);
         textoOperacion = findViewById(R.id.textView);
 
+
+        //llamamos a los metodos cuando se aprete a un boton de los creados
         ejecutarBoton(btn0);
         ejecutarBoton(btn1);
         ejecutarBoton(btn2);
@@ -44,31 +53,77 @@ public class MainActivity extends AppCompatActivity {
         ejecutarBoton(btn7);
         ejecutarBoton(btn8);
         ejecutarBoton(btn9);
+        ejecutarBoton(btnMas);
+        limpiarCalculadora(btnC);
+        resultadoOperacion(btnIgual);
     }
 
+    //metodo que ejecura el metodo onClick del boton que se le pasa por parametro
     public void ejecutarBoton(Button b){
-        System.out.println("Fallo 2");
+
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 anidarOperacion(b.getText().toString());
-            }
+            }//llamamos al metodo que nos añadira el valor a la cadena de la operacion
         });
     }
 
+    //metodo que ira creando la operacion para calcularla mas adelante
     public void anidarOperacion(String valor){
-        System.out.println("FALLO 3");
-        if(!valor.equals("+")){
+
+        //comparamos que si el valor es una + ya que si es asi tendremos que comprobar antes de insertarlo en la operacion
+        if(valor.equals("+")){
+            //si la lista no esta vacia querra decir que ya habra algun numero delante,
+            //asi que podermos realizar la ultima comprobacion
             if(!this.operacion.isEmpty()){
-                if(!(this.operacion.get(this.operacion.size()-1) == "+")){
+                //si el ultimo valor de la lista no es una + podremos añadir el valor +
+                //ya que no pueden haber dos + seguidas
+                if(!this.operacion.get(this.operacion.size()-1).equals("+")){
+                    //cuando pase todas las comprobaciones añadimos el + tanto a el array como a el String
                     this.operacion.add(valor);
                     muestraOperacion += valor;
                 }
             }
+        }else{
+            //si el valor no es + no tendremos problemas, simplemente añadimos el numero a la cadena
+            this.operacion.add(valor);
+            muestraOperacion += valor;
         }
-
+        //cambiamos el texto de el textView
         textoOperacion.setText(muestraOperacion);
-
     }
 
+    //metodo para limpiar la calculadora
+    public void limpiarCalculadora(Button c){
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //limpiamos el array, quitamos el valor de
+                operacion.clear();
+                muestraOperacion = "";
+                textoOperacion.setText(muestraOperacion);
+            }
+        });
+    }
+
+    //metodo que muestra el resultado de la operacion
+    public void resultadoOperacion(Button igual){
+
+
+        igual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                totalSuma = Arrays.stream(muestraOperacion.split("\\+")).mapToInt(n -> Integer.parseInt(n)).sum();
+
+                muestraOperacion = String.valueOf(totalSuma);
+                textoOperacion.setText(muestraOperacion);
+                operacion.clear();
+                operacion.add(muestraOperacion);
+                totalSuma = 0;
+            }
+        });
+
+    }
 }
