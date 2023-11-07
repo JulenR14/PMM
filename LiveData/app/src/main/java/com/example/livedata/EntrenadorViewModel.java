973647fb066a39597922c2fd635a34321db96a2a
JulenR14 <julen20191414@gmail.com>
2023-11-07 18:20:58 +0100

    Nuevo ejercicio Practica 8 : Live Data

---
+++ LiveData/app/src/main/java/com/example/livedata/EntrenadorViewModel.java
@@ -0,0 +1,73 @@
+package com.example.livedata;
+
+import android.app.Application;
+import android.media.Image;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+
+import java.util.function.Function;
+
+public class EntrenadorViewModel extends AndroidViewModel {
+
+    Entrenador entrenador;
+
+    LiveData<Integer> ejercicioLiveData;
+    LiveData<String> repeticionLiveData;
+
+    public EntrenadorViewModel(@NonNull Application application) {
+        super(application);
+
+        entrenador = new Entrenador();
+
+        ejercicioLiveData = (LiveData<Integer>)  Transformations.switchMap(entrenador.ordenLiveData, new Function<String, LiveData<Integer>>(){
+
+            String ejercicioAnterior;
+
+            @Override
+            public LiveData<Integer> apply(String order) {
+                String ejercicio = order.split(":")[0];
+
+                if (!ejercicio.equals(ejercicioAnterior)) {
+                    ejercicioAnterior = ejercicio;
+                    int imagen;
+                    switch (ejercicio) {
+                        case "EJERCICIO1":
+                        default:
+                            imagen = R.drawable.e1;
+                            break;
+                        case "EJERCICIO2":
+                            imagen = R.drawable.e2;
+                            break;
+                        case "EJERCICIO3":
+                            imagen = R.drawable.e3;
+                            break;
+                        case "EJERCICIO4":
+                            imagen = R.drawable.e4;
+                            break;
+                    }
+                    return new MutableLiveData<>(imagen);
+                }
+                return null;
+            }
+        });
+
+        repeticionLiveData = (LiveData<String>) Transformations.switchMap(entrenador.ordenLiveData, new Function<String, LiveData<String>>() {
+            @Override
+            public LiveData<String> apply(String orden) {
+                return new MutableLiveData<>(orden.split(":")[1]);
+            }
+        });
+    }
+
+    LiveData<Integer> obtenerEjercicio(){
+        return ejercicioLiveData;
+    }
+
+    LiveData<String> obtenerRepeticion(){
+        return repeticionLiveData;
+    }
+}
