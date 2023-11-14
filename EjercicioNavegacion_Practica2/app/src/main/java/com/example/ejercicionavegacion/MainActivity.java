db165bf1a77b57071806c4a51087d2609ee1fef2
JulenR14 <julen20191414@gmail.com>
2023-11-14 18:15:03 +0100

    He empezado la practica de la actividad

--- EjercicioNavegacion_Practica2/app/src/main/java/com/example/ejercicionavegacion/MainActivity.java
+++ EjercicioNavegacion_Practica2/app/src/main/java/com/example/ejercicionavegacion/MainActivity.java
@@ -1,49 +1,28 @@
 package com.example.ejercicionavegacion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.ejercicionavegacion.databinding.ActivityBottonBinding;
 import com.example.ejercicionavegacion.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
-
+    ActivityBottonBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
+        setContentView((binding = ActivityBottonBinding.inflate(getLayoutInflater())).getRoot());
 
         setSupportActionBar(binding.toolbar);
 
-        binding.actionGotoDrawerActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, DrawerActivity.class));
-            }
-        });
-
-        binding.actionGotoBottomActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, BottonActivity.class));
-            }
-        });
-
-        binding.actionGotoOptionsActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, OptionsActivity.class));
-            }
-        });
-
-        binding.actionGotoTabbedActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, TabbedActivity.class));
-            }
-        });
+        NavController navController = ((NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment)).getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
+        NavigationUI.setupWithNavController(binding.toolbar, navController);
     }
 }
