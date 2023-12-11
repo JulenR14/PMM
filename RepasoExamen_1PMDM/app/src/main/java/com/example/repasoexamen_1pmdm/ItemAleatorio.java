4a92c5272ff846c96af24e411f57f55ef6b0e90c
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2023-12-11 23:56:33 +0100

    Terminado la prueba

--- RepasoExamen_1PMDM/app/src/main/java/com/example/repasoexamen_1pmdm/ItemAleatorio.java
+++ RepasoExamen_1PMDM/app/src/main/java/com/example/repasoexamen_1pmdm/ItemAleatorio.java
@@ -1,12 +1,25 @@
 package com.example.repasoexamen_1pmdm;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.transition.Visibility;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.repasoexamen_1pmdm.databinding.FragmentItemAleatorioBinding;
+import com.example.repasoexamen_1pmdm.items.Item;
+import com.example.repasoexamen_1pmdm.items.ModelItems;
+import com.example.repasoexamen_1pmdm.items.ViewModelItems;
+import com.example.repasoexamen_1pmdm.items.mostrarItem;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,51 +27,29 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ItemAleatorio extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ItemAleatorio() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ItemAleatorio.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ItemAleatorio newInstance(String param1, String param2) {
-        ItemAleatorio fragment = new ItemAleatorio();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FragmentItemAleatorioBinding binding;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item_aleatorio, container, false);
+        return (binding = FragmentItemAleatorioBinding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ModelItems modelItems = new ModelItems();
+        List<Item> listaItems = modelItems.getListaItems();
+
+        int numeroAleatorio = (int) (Math.random() * listaItems.size());
+
+        Item itemAleatorio = listaItems.get(numeroAleatorio);
+
+        binding.nameItem.setText(itemAleatorio.getNombre());
+        binding.nombreCategoria.setText(itemAleatorio.getCategoria());
+        binding.cantidadCoste.setText(String.valueOf(itemAleatorio.getPrecio()));
+        binding.efectos.setText(itemAleatorio.getEfectos());
+
     }
 }
