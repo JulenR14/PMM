284cba5a29c0ad02decc66533621b5b7b96353be
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2024-01-17 01:14:52 +0100

    Ejercicio terminado, solo falta README

--- ProyectoComarcas/bin/main.dart
+++ ProyectoComarcas/bin/main.dart
@@ -5,6 +5,41 @@ void main(List<String> arguments) {
   var urlInfoComarca = 'https://node-comarques-rest-server-production.up.railway.app/api/comarques/infocomarca/';
   var urlComarcas = 'https://node-comarques-rest-server-production.up.railway.app/api/comarques/';
 
+  String buscar = '';
+
+  if(arguments.length > 0){
+    if(arguments.length > 2){
+      for(int i = 1; i < arguments.length; i++){
+        i == arguments.length - 1 ? buscar += arguments[i] : buscar += arguments[i] + ' ';
+      }
+    }else{
+      buscar = arguments[1];
+    }
+    switch(arguments[0]){
+      case 'comarcas':
+        print('Buscando comarcas...');
+        var url = urlComarcas + buscar;
+        http.get(Uri.parse(url)).then((res){
+          var comarcas = jsonDecode(res.body);
+          comarcas.forEach((comarca) {
+            print('Comarca: $comarca');
+          });
+        });
+        break;
+
+      case 'infocomarca':
+        print('Buscando informacion de la comarca...');
+        var url = urlInfoComarca + buscar;
+        http.get(Uri.parse(url)).then((res){
+          var comarca = jsonDecode(res.body);
+          Comarca comarcaInfo = Comarca.fromJson(comarca);
+          comarcaInfo.imprimirInfo();
+        });
+        break;
+    }
+  }else{
+    print('No has introducido ninguna comarca');
+  }
   
 }
 
@@ -20,6 +55,14 @@ class Comarca{
 
   //constructor
   Comarca(this.comarca, [this.capital, this.poblacio, this.img, this.desc, this.latitud, this.longitud]);
+  Comarca.fromJson(Map<String, dynamic> json)
+    : comarca = json['comarca'],
+      capital = json['capital'],
+      poblacio = json['poblacio'],
+      img = json['img'],
+      desc = json['desc'],
+      latitud = json['latitud'],
+      longitud = json['longitud'];
 
   //metodo para imprimir la informacion
   void imprimirInfo(){
